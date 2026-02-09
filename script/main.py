6bb35f9b0b1e70999e04e92206afd2a090a47892
Roman-milos <romanmilos8@gmail.com>
2026-02-09 17:27:25 +0100

    jai rajouter partiellement le server

--- script/main.py
+++ script/main.py
@@ -3,6 +3,7 @@ import pygame, sys
 from table import *
 from settings import *
 from menu import *
+from multiserver.network import *
 
 class Game :
     def __init__(self) :
@@ -10,14 +11,18 @@ class Game :
         self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT), pygame.DOUBLEBUF, vsync=1)
         pygame.display.set_caption("Poker 2")
         self.clock = pygame.time.Clock()
-        
+        self.n = Network()
         self.menu = Menu(self.screen)
         self.table = Table()
+        # self.table = self.n.send('get')
 
     def run(self) :
         while True :
             dt = self.clock.tick(0) / 1000.0  # secondes
             events = pygame.event.get()
+            new_table = self.n.send('update')
+            # if new_table :
+            #     self.table = new_table
             for event in events :
                 if event.type == pygame.QUIT :
                     pygame.quit()
@@ -27,6 +32,7 @@ class Game :
                 self.menu.update()
             else :
                 self.table.update(dt)
+            # self.table.draw()
             pygame.display.update()
 
 if __name__ == "__main__" :
